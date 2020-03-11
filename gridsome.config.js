af9fd97cb39fc8c202c46600afdb3b6034f962e3
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-03-11 14:01:09 +0000

    Add sitemap

--- gridsome.config.js
+++ gridsome.config.js
@@ -15,6 +15,24 @@ module.exports = {
   },
 
   plugins: [
+    // https://gridsome.org/plugins/@gridsome/plugin-sitemap
+    {
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        cacheTime: 600000, // default
+        // exclude: ['/exclude-me'],
+        config: {
+          // '/articles/*': {
+          //   changefreq: 'weekly',
+          //   priority: 0.5
+          // },
+          // '/about': {
+          //   changefreq: 'monthly',
+          //   priority: 0.7
+          // }
+        }
+      }
+    },
     {
       // Create posts from markdown files
       use: '@gridsome/source-filesystem',
