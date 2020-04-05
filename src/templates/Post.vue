fec042015522eafa9b1de945c492e3dfe7a80dec
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-04-05 14:01:00 +0100

    Add creator

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -73,6 +73,10 @@ export default {
           content: '@RehanSaeedUK'
         },
         {
+          name: 'twitter:creator',
+          content: '@RehanSaeedUK'
+        },
+        {
           name: 'twitter:title',
           content: this.$page.post.title
         },
