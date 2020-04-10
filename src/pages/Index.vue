cbd17914885819d253986c832e2c84458dcf88ee
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-04-10 06:57:14 +0100

    meta tags

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,9 +12,14 @@
 <static-query>
 query {
   metadata {
+    name
+    language
     url
     author {
       name
+      firstName
+      lastName
+      gender
       twitter
     }
   }
@@ -55,46 +60,34 @@ export default {
   },
   computed: {
     title: function() { return 'Blog'; },
-    description: function() { return `Blog posts and more authored by ${this.$static.metadata.author.name}.`; }
+    description: function() { return `Blog posts and more authored by ${this.$static.metadata.author.name}.`; },
+    image: function() { return this.$static.metadata.url + '/images/hero/Muhammad-Rehan-Saeed-1600x900.jpg'; }
   },
   metaInfo() {
     return {
       title: this.title,
       meta: [
-        {
-          name: 'description',
-          content: this.description
-        },
-        {
-          name: 'author',
-          content: this.$static.metadata.author.name
-        },
-
+        { name: 'description', content: this.description },
+        { name: 'author', content: this.$static.metadata.author.name },
         // Twitter card
-        {
-          name: 'twitter:card',
-          content: 'summary_large_image'
-        },
-        {
-          name: 'twitter:site',
-          content: this.$static.metadata.author.twitter
-        },
-        {
-          name: 'twitter:creator',
-          content: this.$static.metadata.author.twitter
-        },
-        {
-          name: 'twitter:title',
-          content: this.title
-        },
-        {
-          name: 'twitter:description',
-          content: this.description
-        },
-        {
-          name: 'twitter:image',
-          content: this.$static.metadata.url + '/images/hero/Muhammad-Rehan-Saeed-1600x900.jpg'
-        }
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: this.$static.metadata.author.twitter },
+        { name: 'twitter:creator', content: this.$static.metadata.author.twitter },
+        { name: 'twitter:title', content: this.title },
+        { name: 'twitter:description', content: this.description },
+        { name: 'twitter:image', content: this.image },
+        // Open Graph
+        { property: 'og:title', content: this.title },
+        { property: 'og:url', content: this.$static.metadata.url },
+        { property: 'og:image', content: this.image },
+        { property: 'og:description', content: this.description },
+        { property: 'og:locale', content: this.$static.metadata.language.replace('-', '_') },
+        { property: 'og:site_name', content: this.$static.metadata.name },
+        { property: 'og:type', content: 'profile' },
+        { property: 'profile:first_name', content: this.$static.metadata.author.firstName },
+        { property: 'profile:last_name', content: this.$static.metadata.author.lastName },
+        { property: 'profile:username', content: this.$static.metadata.author.name },
+        { property: 'profile:gender', content: this.$static.metadata.author.gender },
       ]
     }
   }
