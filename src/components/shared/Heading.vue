7a37790c4f19a48fcbac21d26499c80a547791f1
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-04-20 10:04:54 +0100

    Add heading

---
+++ src/components/shared/Heading.vue
@@ -0,0 +1,49 @@
+<template>
+  <Component
+    :is="tag || `h${level}`"
+    :class="`heading heading--${size || level}`"
+  >
+    <slot/>
+  </Component>
+</template>
+
+<script>
+export default {
+  props: {
+    level: {
+      default: '1',
+      type: String,
+    },
+    size: {
+      default: null,
+      type: String,
+    },
+    tag: {
+      default: null,
+      type: String,
+    },
+  }
+};
+</script>
+
+<style>
+.heading--1 {
+  font-size: var(--global-font-size-6);
+}
+
+.heading--2 {
+  font-size: var(--global-font-size-5);
+}
+
+.heading--3 {
+  font-size: var(--global-font-size-4);
+}
+
+.heading--4 {
+  font-size: var(--global-font-size-3);
+}
+
+.heading--5 {
+  font-size: var(--global-font-size-2);
+}
+</style>
