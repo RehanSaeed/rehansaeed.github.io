7dd36d698ba021d865052814a71d9e86780cb907
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-04-21 14:27:08 +0100

    Rename

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import '~/assets/style/index.scss';
 
 // Import default layout so we don't need to import it to every page
-import DefaultLayout from '~/layouts/Default.vue';
+import defaultLayout from '~/layouts/default.vue';
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, { router, head, isClient }) {
@@ -37,5 +37,5 @@ export default function (Vue, { router, head, isClient }) {
   });
 
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout);
+  Vue.component('Layout', defaultLayout);
 }
