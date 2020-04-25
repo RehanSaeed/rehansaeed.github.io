40fa989d8ee84b95295096c92124df12406b2ddd
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-04-25 09:22:48 +0100

    Add updated to post meta

--- src/components/post-meta.vue
+++ src/components/post-meta.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="post-meta">
-    Posted {{ meta.displayDate }}
+    Posted <time :datetime="meta.date">{{meta.displayDate}}</time>
+    <span v-if="updated"> and updated <time :datetime="meta.dateModified">{{updated}}</time></span>
     <template v-if="meta.timeToRead">
       - <strong>{{ meta.timeToRead }} min read</strong>
     </template>
@@ -8,12 +9,22 @@
 </template>
 
 <script>
+import { formatDistance } from 'date-fns';
+
 export default {
   name: 'u-post-meta',
   props: {
     meta: {
       type: Object,
     },
+  },
+  computed: {
+    updated: function() {
+      if (this.meta.dateModified) {
+        return formatDistance(new Date(this.meta.dateModified), new Date(), { addSuffix: true });
+      }
+      return undefined;
+    }
   }
 }
 </script>
