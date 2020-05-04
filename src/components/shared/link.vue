8c4aea5079d28a5cc4166aa451e40fda9cfd1f15
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-05-04 19:32:23 +0100

    bordered button improvements

--- src/components/shared/link.vue
+++ src/components/shared/link.vue
@@ -57,17 +57,18 @@ export default {
   background-color: var(--global-background-color);
   border: var(--global-border-width-1) solid var(--global-border-color);
   border-radius: var(--global-border-radius);
-  box-shadow: 1px 1px 1px hsl(0, 0%, 90%);
+  box-shadow: 0 1px 1px hsl(0, 0%, 90%);
   font-size: var(--global-font-size-1);
   padding: var(--global-space-fixed-2) var(--global-space-fixed-4);
 
   &:hover {
-    box-shadow: 3px 3px 3px hsl(0, 0%, 70%);
-    transform: none;
+    box-shadow: 0 3px 5px hsl(0, 0%, 70%);
+    transform: translateY(-2px);
   }
 
   &:active {
     box-shadow: 0 0 0 hsl(0, 0%, 70%);
+    transform: translateY(0);
   }
 }
 
