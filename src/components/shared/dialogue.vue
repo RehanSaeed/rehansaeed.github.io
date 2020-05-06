c8ee2ce38325262a966000f1121bad3a78dcabec
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-05-06 11:30:38 +0100

    backdrop colour

--- src/components/shared/dialogue.vue
+++ src/components/shared/dialogue.vue
@@ -96,8 +96,8 @@ dialog::backdrop,
 dialog + .backdrop {
   position: fixed;
   top: 0; right: 0; bottom: 0; left: 0;
-  background: var(--global-border-color);
-  backdrop-filter: blur(2px);
+  background: hsla(0, 0%, 0%, .2);
+  backdrop-filter: blur(3px);
 }
 
 ._dialog_overlay {
@@ -113,6 +113,7 @@ dialog.fixed {
 
 .dialog {
   background: var(--global-content-background-color);
+  border: var(--global-border-width-2) solid var(--global-border-color);
   border: none;
   border-radius: var(--global-border-radius);
   color: var(--global-title-color);
