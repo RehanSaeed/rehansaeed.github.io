5c67a32ca7be1e23c5b2cc471fc7b258293808fc
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-05-15 09:09:58 +0100

    About

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -9,6 +9,7 @@
         <u-heading level="2" class="about__title">Muhammad Rehan Saeed</u-heading>
         <u-social-links class="about__social-links"/>
         <p class="about__description">Rehan is a professional Software Developer at Microsoft. Although he works for Microsoft his opinions are his own. If it’s written in C# or .NET, Rehan has probably written something using it in anger!</p>
+        <p class="about__description">You can see some of the open source projects that I've started and maintained in my <g-link to="/portfolio/">portfolio</g-link>. There are many others I've contributed to which you can see in my <a href="https://github.com/RehanSaeed">GitHub profile</a> and of course there are other commercial projects that I cannot disclose.</p>
         <p class="about__description">You can see a timeline of my open source work and blog posts in my <a href="https://stackoverflow.com/story/muhammad-rehan-saeed">Stack Overflow Developer Story</a> or just view my <a href="https://stackoverflow.com/users/1212017/muhammad-rehan-saeed">Stack Overflow profile</a>.</p>
         <p class="about__description">Do you have questions or comments about my work? Please feel free to contact me using any of the links above.</p>
       </u-content-box>
