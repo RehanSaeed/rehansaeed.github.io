098e335d266cdfa4069a67c07d690814d9955265
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-06-08 12:25:47 +0100

    Add date to reposts

--- src/components/webmention-faces.vue
+++ src/components/webmention-faces.vue
@@ -2,10 +2,10 @@
   <div class="webmention-faces">
     <u-avatar
       class="webmention-faces__face"
-      v-for="mention in mentions"
+      v-for="{ mention, label } in mentionsDisplay"
       :key="mention.id"
       :href="mention.data.url"
-      :label="mention.data.author.name"
+      :label="label"
       :alt="mention.data.author.name"
       :src="mention.data.author.photo"/>
   </div>
@@ -13,6 +13,7 @@
 
 <script>
 import avatar from '~/components/shared/avatar.vue';
+import { getDisplayDateFromString } from '~/framework/date.js';
 
 export default {
   name: 'u-webmention-faces',
@@ -23,6 +24,22 @@ export default {
     mentions: {
       required: true,
       type: Array,
+    }
+  },
+  methods: {
+    getLabel(mention) {
+      if (mention.data.published) {
+        return `${mention.data.author.name} - ${getDisplayDateFromString(mention.data.published)}`;
+      }
+      return mention.data.author.name;
+    }
+  },
+  computed: {
+    mentionsDisplay() {
+      return this.mentions.map(mention => ({
+        mention,
+        label: this.getLabel(mention),
+      }));
     }
   }
 }
