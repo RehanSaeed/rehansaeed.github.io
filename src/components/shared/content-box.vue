3f0a524030d70bed4c0df67bff0c66a792807ebf
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-07-24 13:35:14 +0100

    box shadow with better performance

--- src/components/shared/content-box.vue
+++ src/components/shared/content-box.vue
@@ -1,31 +1,34 @@
 <template>
   <div class="content-box" :is="tag" :class="{ 'content-box--hoverable': hoverable }">
-    <slot/>
+    <slot />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'u-content-box',
+  name: "u-content-box",
   props: {
     hoverable: {
       type: Boolean,
     },
     tag: {
-      default: 'div',
+      default: "div",
       type: String,
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 .content-box {
   background-color: var(--global-content-background-color);
   border-radius: var(--global-border-radius);
-  box-shadow: 1px 1px 5px 0 rgba(0,0,0,.02), 1px 1px 15px 0 rgba(0,0,0,.03);
+  box-shadow: 1px 1px 5px 0 rgba(0, 0, 0, 0.02),
+    1px 1px 15px 0 rgba(0, 0, 0, 0.03);
   padding: var(--global-space-fluid-6);
-  transition: transform var(--global-duration-2) ease-out, background-color var(--global-duration-2) ease-out, box-shadow var(--global-duration-3) ease-out;
+  transition: transform var(--global-duration-2) ease-out,
+    background-color var(--global-duration-2) ease-out,
+    box-shadow var(--global-duration-3) ease-out;
 
   // @mixin word-wrap()
   overflow-wrap: break-word;
@@ -35,9 +38,23 @@ export default {
 }
 
 .content-box--hoverable {
-  &:hover {
-    transform: translateY(-5px);
-    box-shadow: 1px 10px 30px 0 rgba(0,0,0,.1);
-  }
+  position: relative;
+}
+
+.content-box--hoverable::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  box-shadow: 1px 10px 30px 0 rgba(0, 0, 0, 0.2);
+  opacity: 0;
+  transition: opacity 500ms;
+}
+
+.content-box--hoverable:hover::before {
+  opacity: 1;
 }
 </style>
