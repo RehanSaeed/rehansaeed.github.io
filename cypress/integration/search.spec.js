fbecdef9090ff736fa174928a9c28cb7eea05031
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2020-12-15 12:26:50 +0000

    Add cypress tests

---
+++ cypress/integration/search.spec.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+context("Smoke", () => {
+  it("Open search", () => {
+    cy.visit("/");
+    cy.get("nav").contains("Search").click();
+
+    cy.get("input#search").should("be.visible");
+  });
+
+  it("Close search", () => {
+    cy.visit("/");
+    cy.get("nav").contains("Search").click();
+    cy.get("button.dialog__close").click();
+
+    cy.get("input#search").should("not.be.visible");
+  });
+
+  it("Search portfolio", () => {
+    cy.visit("/");
+    cy.get("nav").contains("Search").click();
+    cy.get("input#search").type("Schema.NET");
+
+    cy.get(".search__result")
+      .first()
+      .should("have.attr", "href", "https://github.com/RehanSaeed/Schema.NET");
+  });
+
+  it("Search post", () => {
+    cy.visit("/");
+    cy.get("nav").contains("Search").click();
+    cy.get("input#search").type("ASP.NET");
+    cy.get(".search__result").first().click();
+
+    cy.get("input#search").should("not.be.visible");
+  });
+});
---
+++ cypress/integration/smoke.spec.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+const disableSmoothScroll = () => {
+  cy.document().then((document) => {
+    const node = document.createElement("style");
+    node.innerHTML = "html { scroll-behavior: inherit !important; }";
+    document.body.appendChild(node);
+  });
+};
+
+context("Smoke", () => {
+  it("Visit all pages", () => {
+    cy.get("html").invoke("attr", "style", "scroll-behavior: inherit");
+
+    cy.visit("/");
+    disableSmoothScroll();
+    cy.get("h1#muhammad-rehan-saeed").should("be.visible");
+
+    cy.get("nav").contains("Portfolio").click();
+    cy.get("h1#portfolio").should("be.visible");
+
+    cy.get("nav").contains("About").click();
+    cy.get("h1#about").should("be.visible");
+
+    cy.get("nav").contains("Blog").click();
+    cy.get("h1#muhammad-rehan-saeed").should("be.visible");
+
+    cy.get(".post-card__title").first().click();
+    cy.get("#muhammad-rehan-saeed").should("not.be.visible");
+
+    cy.get(".tags__link").first().click();
+    cy.get("h1").should("contain", "#");
+  });
+
+  it("404 Page", () => {
+    cy.visit("/this-does-not-exist");
+    cy.get("h1").contains("404 Not Found").should("be.visible");
+  });
+});
