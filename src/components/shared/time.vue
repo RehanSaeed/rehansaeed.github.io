85ebdce9239e763aa461a47546f8a8edd41cca54
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-01-22 17:01:39 +0000

    Add time component

---
+++ src/components/shared/time.vue
@@ -0,0 +1,24 @@
+<template>
+  <time class="time" :datetime="datetime" :title="datetime">{{
+    datetimeDisplay
+  }}</time>
+</template>
+
+<script>
+import { getDisplayDateFromString } from "~/framework/date.js";
+
+export default {
+  name: "u-time",
+  props: {
+    datetime: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    datetimeDisplay() {
+      return getDisplayDateFromString(this.datetime);
+    },
+  },
+};
+</script>
