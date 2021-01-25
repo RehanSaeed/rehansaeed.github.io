c2efc3b803fc277ff3645e43e682b69a3bd42ef9
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-01-25 11:04:11 +0000

    Add search-result.vue

---
+++ src/components/search-result.vue
@@ -0,0 +1,77 @@
+<template>
+  <article class="search-result">
+    <g-link
+      @click.native="onSelected"
+      :to="searchResult.permalink || searchResult.path"
+      class="search-result__link"
+    >
+      <g-image
+        v-if="searchResult.node.heroImage"
+        class="search-result__image"
+        :src="searchResult.node.heroImage"
+      />
+      <u-heading
+        :id="searchResult.node.title"
+        class="search-result__title"
+        level="3"
+        >{{ searchResult.node.title }}</u-heading
+      >
+      <p class="search-result__description">
+        {{ searchResult.node.description }}
+      </p>
+    </g-link>
+  </article>
+</template>
+
+<script>
+import Heading from "./shared/heading.vue";
+
+export default {
+  name: "u-search-result",
+  components: {
+    "u-heading": Heading,
+  },
+  props: {
+    searchResult: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    onSelected() {
+      this.$emit("selected");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.search-result__link {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-areas:
+    "image title"
+    "image description";
+  text-decoration: none;
+}
+.search-result__image {
+  grid-area: image;
+  border: var(--global-border-width-2) solid var(--global-border-color);
+  border-radius: var(--global-border-radius);
+  margin-right: var(--global-space-fixed-5);
+  min-width: 8rem;
+  width: 8rem;
+}
+.search-result__title {
+  grid-area: title;
+  color: var(--global-title-color);
+  font-size: var(--global-font-size-3);
+  margin: 0;
+}
+.search-result__description {
+  grid-area: description;
+  color: var(--global-body-color);
+  font-size: var(--global-font-size-1);
+  margin: 0;
+}
+</style>
