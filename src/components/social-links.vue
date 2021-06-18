b9db3e47def8fb85abe18c9ef6c7619e27b33429
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-06-18 17:14:28 +0100

    Add twitch link

--- src/components/social-links.vue
+++ src/components/social-links.vue
@@ -45,6 +45,15 @@
     >
       <u-icon-youtube :size="30" />
     </u-link-button>
+    <u-link-button
+      v-if="this.$static.metadata.author.twitch.url"
+      label="Twitch"
+      :href="this.$static.metadata.author.twitch.url"
+      class="social__link"
+      rel="me"
+    >
+      <u-icon-twitch :size="30" />
+    </u-link-button>
     <u-link-button class="social__link" label="Email" :href="mailTo">
       <u-icon-email :size="30" />
     </u-link-button>
@@ -61,6 +70,7 @@ import iconGitHub from "~/components/shared/icons/icon-github.vue";
 import iconLinkedIn from "~/components/shared/icons/icon-linkedin.vue";
 import iconRss from "~/components/shared/icons/icon-rss.vue";
 import iconStackOverflow from "~/components/shared/icons/icon-stackoverflow.vue";
+import iconTwitch from "~/components/shared/icons/icon-twitch.vue";
 import iconTwitter from "~/components/shared/icons/icon-twitter.vue";
 import iconYoutube from "~/components/shared/icons/icon-youtube.vue";
 import { decode } from "~/framework/obfuscate.js";
@@ -74,6 +84,7 @@ export default {
     "u-icon-linkedin": iconLinkedIn,
     "u-icon-rss": iconRss,
     "u-icon-stackoverflow": iconStackOverflow,
+    "u-icon-twitch": iconTwitch,
     "u-icon-twitter": iconTwitter,
     "u-icon-youtube": iconYoutube,
   },
@@ -103,6 +114,9 @@ query {
         url
       }
       stackOverflow {
+        url
+      }
+      twitch {
         url
       }
       twitter {
