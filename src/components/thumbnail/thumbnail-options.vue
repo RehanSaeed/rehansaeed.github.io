ace2f124b446f37c3d31a1ec1ea096dd97247dd6
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-07-01 11:57:15 +0100

    Add thumbnail-options

---
+++ src/components/thumbnail/thumbnail-options.vue
@@ -0,0 +1,71 @@
+<template>
+  <aside class="thumbnail-options">
+    <h2 class="thumbnail-options__title">General</h2>
+    <section class="thumbnail-options__section">
+      <fieldset class="thumbnail-options__fieldset">
+        <label class="thumbnail-options__label">background</label>
+        <input
+          class="thumbnail-options__input"
+          placeholder="none|light|dark|/images/hero/Code-1600x900.jpg"
+          :value="background"
+          @input="$emit('update:background', $event.target.value)"
+        />
+      </fieldset>
+    </section>
+
+    <h2 class="thumbnail-options__title">Title</h2>
+    <ul>
+      <li><em>title</em> - Title</li>
+      <li><em>subtitle</em> - Subtitle</li>
+      <li><em>title-font-size</em> - 5</li>
+      <li><em>title-justify</em> - start, center, end</li>
+      <li><em>title-align</em> - start, center, end</li>
+      <li><em>title-width</em> - 300</li>
+    </ul>
+
+    <h2 class="thumbnail-options__title">Logo</h2>
+    <ul>
+      <li><em>logo</em></li>
+      <li><em>logo-justify</em> - start, center, end</li>
+      <li><em>logo-align</em> - start, center, end</li>
+    </ul>
+
+    <h2 class="thumbnail-options__title">Image 1</h2>
+    <ul>
+      <li><em>image1</em> - /images/hero/Code-1600x900.jpg</li>
+      <li><em>image1-rotate</em> - 0-360</li>
+      <li><em>image1-width</em> - 600</li>
+      <li><em>image1-height</em> - 600</li>
+      <li><em>image1-x</em> - 950</li>
+      <li><em>image1-y</em> - 200</li>
+    </ul>
+
+    <h2 class="thumbnail-options__title">Image 2</h2>
+    <ul>
+      <li><em>image2</em> - /images/hero/Code-1600x900.jpg</li>
+      <li><em>image2-rotate</em> - 0-360</li>
+      <li><em>image2-width</em> - 600</li>
+      <li><em>image2-height</em> - 600</li>
+      <li><em>image2-x</em> - 950</li>
+      <li><em>image2-y</em> - 200</li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+  props: {
+    background: {
+      required: true,
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.thumbnail-options__fieldset {
+  display: flex;
+  gap: var(--global-space-fluid-2);
+}
+</style>
--- src/pages/thumbnail.vue
+++ src/pages/thumbnail.vue
@@ -10,56 +10,10 @@
       <u-thumbnail-image class="thumbnail__image1" name="image1" />
       <u-thumbnail-image class="thumbnail__image2" name="image2" />
     </article>
-    <aside>
-      <h2>General</h2>
-      <section>
-        <fieldset class="thumbnail__fieldset">
-          <label class="thumbnail__label">background</label>
-          <input
-            class="thumbnail__input"
-            v-model="background"
-            placeholder="none|light|dark|/images/hero/Code-1600x900.jpg"
-          />
-        </fieldset>
-      </section>
-
-      <h2>Title</h2>
-      <ul>
-        <li><em>title</em> - Title</li>
-        <li><em>subtitle</em> - Subtitle</li>
-        <li><em>title-font-size</em> - 5</li>
-        <li><em>title-justify</em> - start, center, end</li>
-        <li><em>title-align</em> - start, center, end</li>
-        <li><em>title-width</em> - 300</li>
-      </ul>
-
-      <h2>Logo</h2>
-      <ul>
-        <li><em>logo</em></li>
-        <li><em>logo-justify</em> - start, center, end</li>
-        <li><em>logo-align</em> - start, center, end</li>
-      </ul>
-
-      <h2>Image 1</h2>
-      <ul>
-        <li><em>image1</em> - /images/hero/Code-1600x900.jpg</li>
-        <li><em>image1-rotate</em> - 0-360</li>
-        <li><em>image1-width</em> - 600</li>
-        <li><em>image1-height</em> - 600</li>
-        <li><em>image1-x</em> - 950</li>
-        <li><em>image1-y</em> - 200</li>
-      </ul>
-
-      <h2>Image 2</h2>
-      <ul>
-        <li><em>image2</em> - /images/hero/Code-1600x900.jpg</li>
-        <li><em>image2-rotate</em> - 0-360</li>
-        <li><em>image2-width</em> - 600</li>
-        <li><em>image2-height</em> - 600</li>
-        <li><em>image2-x</em> - 950</li>
-        <li><em>image2-y</em> - 200</li>
-      </ul>
-    </aside>
+    <u-thumbnail-options
+      :background="background"
+      @update:background="background = $event"
+    />
   </Layout>
 </template>
 
@@ -67,6 +21,7 @@
 import Layout from "~/layouts/empty.vue";
 import thumbnailImage from "~/components/thumbnail/thumbnail-image.vue";
 import thumbnailLogo from "~/components/thumbnail/thumbnail-logo.vue";
+import thumbnailOptions from "~/components/thumbnail/thumbnail-options.vue";
 import thumbnailTitle from "~/components/thumbnail/thumbnail-title.vue";
 
 export default {
@@ -74,6 +29,7 @@ export default {
     Layout,
     "u-thumbnail-image": thumbnailImage,
     "u-thumbnail-logo": thumbnailLogo,
+    "u-thumbnail-options": thumbnailOptions,
     "u-thumbnail-title": thumbnailTitle,
   },
   data() {
@@ -126,10 +82,5 @@ export default {
 .thumbnail__image1,
 .thumbnail__image2 {
   grid-area: content;
-}
-
-.thumbnail__fieldset {
-  display: flex;
-  gap: var(--global-space-fluid-2);
 }
 </style>
