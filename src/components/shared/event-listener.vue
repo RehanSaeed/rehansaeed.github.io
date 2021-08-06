7c8efad9dc4472b103ae086e9a23752127209ae6
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2021-08-06 18:02:47 +0100

    Add event listener component

---
+++ src/components/shared/event-listener.vue
@@ -0,0 +1,24 @@
+<script>
+/*
+ * @example
+ * <u-event-listener
+ *   @keydown.meta.70="foo"
+ *   @keydown.ctrl.70="bar"
+ *   @keydown.space="baz"/>
+ */
+export default {
+  name: "u-event-listener",
+  mounted() {
+    Object.keys(this.$listeners).forEach((event) => {
+      const handler = this.$listeners[event];
+      window.addEventListener(event, handler);
+    });
+  },
+  destroyed() {
+    Object.keys(this.$listeners).forEach((event) => {
+      const handler = this.$listeners[event];
+      window.removeEventListener(event, handler);
+    });
+  },
+};
+</script>
