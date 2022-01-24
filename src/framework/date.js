51a79dcd303497c5fa433c144f317bcd95808446
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2022-01-24 15:16:43 +0000

    Use less date-fns

--- src/framework/date.js
+++ src/framework/date.js
@@ -1,13 +1,25 @@
-import { differenceInDays, format, formatDistance, getYear } from "date-fns";
+import { differenceInDays, formatDistance } from "date-fns";
+
+const shortDateTimeFormat = new Intl.DateTimeFormat("en-GB", {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
+
+const longDateTimeFormat = new Intl.DateTimeFormat("en-GB", {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
 
 export function getDisplayDate(date) {
   const now = new Date();
   if (differenceInDays(now, date) <= 30) {
     return formatDistance(date, now, { addSuffix: true });
-  } else if (getYear(now) == getYear(date)) {
-    return format(date, "d MMMM");
+  } else if (now.getFullYear() == date.getFullYear()) {
+    return shortDateTimeFormat.format(date);
   } else {
-    return format(date, "d MMMM yyyy");
+    return longDateTimeFormat.format(date);
   }
 }
 
